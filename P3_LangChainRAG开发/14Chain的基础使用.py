bb16a712e3b2a20ffc2dd9c417513683fc5261eb
shuchangwang <1326125375@qq.com>
2026-08-21 15:43:32 +0800

    新增：学习Langchain的Chain链基础使用

--- P3_LangChainRAG开发/14Chain的基础使用.py
+++ P3_LangChainRAG开发/14Chain的基础使用.py
@@ -15,22 +15,24 @@ history_data = [
     ("human", "好诗再来一个"),
     ("ai", "锄禾日当午，汗滴禾下锄，谁知盘中餐，粒粒皆辛苦"),
 ]
-prompt_text = chat_prompt_template.invoke({"history": history_data}).to_string()
-"""
-System: 你是一个边塞诗人，可以作诗。
-Human: 你来写一个唐诗
-AI: 床前明月光，疑是地上霜，举头望明月，低头思故乡
-Human: 好诗再来一个
-AI: 锄禾日当午，汗滴禾下锄，谁知盘中餐，粒粒皆辛苦
-Human: 请再来一首唐诗
-"""
 
 llm = ChatOpenAI(
     model_name="qwen3.8-max",
     base_url="https://llm-lh6s7em3q8okhlai.cn-beijing.maas.aliyuncs.com/compatible-mode/v1",
     temperature=0
 )
-result = llm.invoke(input=prompt_text)
-print(type(result))  # <class 'langchain_core.messages.ai.AIMessage'>
-print(result)
-print(result.content)
+
+# 组成链，要求每一个组件都是Runnable接口的子类
+chain = chat_prompt_template | llm
+print(type(chain))  # <class 'langchain_core.runnables.base.RunnableSequence'>
+print(chain)
+
+# 通过链去调用invoke或stream
+# result = chain.invoke(input={"history": history_data})
+# print(type(result))  # <class 'langchain_core.messages.ai.AIMessage'>
+# print(result)
+# print(result.content)
+
+# # 通过stream流式输出
+# for chunk in chain.stream({"history": history_data}):
+#     print(chunk.content, end="", flush=True)
